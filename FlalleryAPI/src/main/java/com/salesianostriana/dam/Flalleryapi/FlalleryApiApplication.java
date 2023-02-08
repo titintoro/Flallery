c4e63b904711a75e8ae55891e26b5bf65a48706f
titintoro <titintola@gmail.com>
2023-02-08 13:09:41 +0100

    Creating Projects

---
+++ FlalleryAPI/src/main/java/com/salesianostriana/dam/Flalleryapi/FlalleryApiApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.Flalleryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlalleryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlalleryApiApplication.class, args);
+	}
+
+}
